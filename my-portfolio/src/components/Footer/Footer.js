175371f00500643d498d3cb999f5924d475bb697
1997Ricardo <ricardosunarodriguez@gmail.com>
2025-05-30 13:32:54 +0200

    prueba logo X

--- my-portfolio/src/components/Footer/Footer.js
+++ my-portfolio/src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ export const Footer = () => `
 
 <h2>Contact</h2>
 <div>
-${Button("/public/twitter.png", "Twitter","https://x.com/ricardosuna_?s=21&t=t5cWmIsh7_0yRx3xo5Aq5g")}
+${Button("https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/X_logo_2023.svg/1920px-X_logo_2023.svg.png", "Twitter","https://x.com/ricardosuna_?s=21&t=t5cWmIsh7_0yRx3xo5Aq5g")}
 ${Button("/public/github.jpeg", "GitHub","https://github.com/1997Ricardo")}
 ${Button("/public/linkedin.jpeg", "LinkedIn","https://www.linkedin.com/in/ricardo-osuna-rodr%C3%ADguez-a48219227?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app")}
 ${Button("/public/telegram.jpg", "Telegram")}
