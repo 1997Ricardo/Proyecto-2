d61565b28d55b6a4736959afaa8d2b66f45443eb
1997Ricardo <ricardosunarodriguez@gmail.com>
2025-05-30 13:17:43 +0200

    pequeñas modificaciones

--- my-portfolio/src/pages/About.js
+++ my-portfolio/src/pages/About.js
@@ -9,6 +9,7 @@ export const About = () => {
   main.innerHTML = `
     <section class="about container">
       <h2>About me</h2>
+      ${Divider()}
       <div class="about__content">
         <img 
           src="https://avatars.githubusercontent.com/u/182170683?v=4" 
@@ -18,7 +19,7 @@ export const About = () => {
         <div class="about__text">
 <p>¡Hola! Soy <strong>Ricardo Osuna</strong>, licenciado en Ciencias de la Actividad Física y del Deporte y formándome como Programador Full-Stack y Técnico DAM. Tras varios másteres en educación, nutrición y readaptación de lesiones, he volcado mi capacidad de análisis y detalle al mundo del desarrollo web y la gestión de bases de datos.</p>
 
-<p>Me apasiona diseñar experiencias digitales que conjuguen rendimiento y estética, aplicando metodologías basadas en datos —tal como hago entrenando a atletas de alto rendimiento en crossfit y carreras híbridas— para optimizar cada proyecto con estrategias de mejora continua.</p>
+<p>Me apasiona diseñar experiencias digitales que conjuguen rendimiento y estética, aplicando metodologías basadas en datos tal como hago entrenando a atletas de alto rendimiento en crossfit y carreras híbridas para optimizar cada proyecto con estrategias de mejora continua.</p>
 
 <p>Puedes descargar mi <a href="/cvRicardo.pdf" target="_blank" rel="noopener noreferrer">CV completo en PDF</a> o contactarme directamente por <a href="mailto:lionbeat.adm@gmail.com">correo</a> o vía GitHub: <a href="https://github.com/1997Ricardo" target="_blank" rel="noopener noreferrer">github.com/1997Ricardo</a>.</p>
 
--- my-portfolio/src/pages/Home.js
+++ my-portfolio/src/pages/Home.js
@@ -6,7 +6,7 @@ export const Home = () => {
   cleanPage(main);
   main.innerHTML = `
     <section class="home">
-    <p>Hi, I'm</p>
+    
     <h1>Ricardo Osuna</h1>
     <p> I’m a Full-Stack Developer trainee at The Power MBA with a DAM technical degree and a degree in Sports Sciences—plus master’s in Teaching, Performance, Nutrition, and Injury Readaptation, where I honed problem-solving and data-management skills. Since November 2022, I’ve led CrossFit & HIFT coaching at Culture Fitness Mijas, applying data-driven training and team leadership—abilities I transfer directly to managing IT projects. Fluent in Spanish and intermediate in English, I’m proficient with MySQL Workbench, VS Code, Git/GitHub, Trello/Notion, Figma and Google Workspace.
  </p>
